7bda97503cd69fee979c7558fa45969038096c7c
fujii dan <2607dan@gmail.com>
2021-03-31 17:39:23 +0900

    webpackerについて

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,7 @@
 const { environment } = require('@rails/webpacker')
+const erb = require('./loaders/erb')
+const provide = require('./plugins/provide')
+environment.plugins.prepend('provide', provide)
 
+environment.loaders.prepend('erb', erb)
 module.exports = environment
